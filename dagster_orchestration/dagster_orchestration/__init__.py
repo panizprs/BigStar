b6d6c1962758562b3c785b378d59387779804dfb
Paniz <paniz@eduroam-193-157-237-212.wlan.uio.no>
2024-02-20 12:08:23 +0100

    dagster

--- dagster_orchestration/dagster_orchestration/__init__.py
+++ dagster_orchestration/dagster_orchestration/__init__.py
@@ -1,9 +1,21 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import (Definitions,
+                     load_assets_from_modules,
+                     AssetSelection,
+                     ScheduleDefinition,
+                     define_asset_job)
 
-from . import assets
+from .assets import resources
 
-all_assets = load_assets_from_modules([assets])
+# Define a job that will materialize the assets
+big_star_job = define_asset_job("big_star_job", selection=AssetSelection.all())
 
-defs = Definitions(
-    assets=all_assets,
+# A ScheduleDefinition the job it should run and a cron schedule of how frequently to run it
+big_star_schedule = ScheduleDefinition(
+    job=big_star_job,
+    cron_schedule="0 * * * *",  # every hour
 )
+
+defs = Definitions(assets=load_assets_from_modules([assets]),
+                   resources=resources,
+                   schedules = [big_star_schedule],
+    )
